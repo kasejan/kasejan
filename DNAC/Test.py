3f86bdb8ef4a375415413d624312c24ebbd896b8
Jan Kase <Jan.Kase@alef.com>
2022-07-19 10:53:03 +0200

    line deleting

--- DNAC/Test.py
+++ DNAC/Test.py
@@ -1,12 +1,6 @@
 from dnacentersdk import DNACenterAPI
-
-
-
 from env import DNA_CENTER_USERNAME, DNA_CENTER_PASSWORD, DNA_CENTER_BASE_URL, DNA_CENTER_VERSION, DNA_CENTER_VERIFY
 from Design.Network_Settings import Device_Credentials
-
-
-
 
 api = DNACenterAPI(DNA_CENTER_USERNAME, DNA_CENTER_PASSWORD, base_url=DNA_CENTER_BASE_URL, version=DNA_CENTER_VERSION, verify=DNA_CENTER_VERIFY)
 
